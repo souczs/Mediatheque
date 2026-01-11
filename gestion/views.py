edf66500ca3a39e3441354bccf54aff35b2168a3
Sonia EL BOURI <soniaelbouri@Mac.lan>
2026-01-11 17:26:27 +0100

    HTML

--- gestion/views.py
+++ gestion/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Media
 
 # Create your views here.
+def home(request):
+    tous_les_medias = Media.objects.all()
+    return render(request, 'gestion/home.html', {'medias': tous_les_medias})
